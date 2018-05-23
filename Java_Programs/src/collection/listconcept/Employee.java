5a60996e4a639e959f392f174944d885cd6785f5
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2018-05-23 22:45:02 +0530

    Updated on May-23-2018

---
+++ Java_Programs/src/collection/listconcept/Employee.java
@@ -0,0 +1,22 @@
+package collection.listconcept;
+
+/**
+ *
+ * @author Sanjeev Chaudhary 
+ * https://github.com/sanjeevss3/
+ *
+ */
+
+public class Employee {
+
+	String name;
+	int age;
+	String dept;
+
+	Employee(String name, int age, String dept) {
+		this.name = name;
+		this.age = age;
+		this.dept=dept;
+	}
+
+}
